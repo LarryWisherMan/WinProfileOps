18f3020fc62054c7418156f3bb4e41e1a039a1b9
LarryWisherMan <MikeRow@upkep.net>
2024-09-23 16:53:11 -0700

    add RemoveProfileReg function `New-ProfileDeletionResult`

--- source/Private/RemoveProfileReg/New-ProfileDeletionResult.ps1
+++ source/Private/RemoveProfileReg/New-ProfileDeletionResult.ps1
@@ -1,24 +1,57 @@
+<#
+.SYNOPSIS
+Creates a new `ProfileDeletionResult` object with details of a user profile deletion.
+
+.DESCRIPTION
+The `New-ProfileDeletionResult` function generates a new object representing the outcome of a user profile deletion operation. This object can include details such as the SID, profile path, deletion status, and computer name.
+
+.PARAMETER SID
+Specifies the Security Identifier (SID) of the user profile.
+
+.PARAMETER ProfilePath
+Specifies the path to the user profile that was deleted (optional).
+
+.PARAMETER DeletionSuccess
+Specifies whether the profile deletion was successful.
+
+.PARAMETER DeletionMessage
+Provides a message regarding the profile deletion result.
+
+.PARAMETER ComputerName
+Specifies the name of the computer from which the profile was removed.
+
+.EXAMPLE
+New-ProfileDeletionResult -SID 'S-1-5-21-...' -DeletionSuccess $true -DeletionMessage 'Profile removed successfully.'
+
+Description:
+Creates a `ProfileDeletionResult` object indicating that the profile for the specified SID was successfully removed.
+
+.OUTPUTS
+ProfileDeletionResult object containing the details of the deletion operation.
+#>
 function New-ProfileDeletionResult
 {
     [CmdletBinding(DefaultParameterSetName = 'Minimal')]
     param (
-        # Parameter set 1: Full constructor with all parameters
+        # SID is mandatory in all parameter sets
         [Parameter(Mandatory = $true, ParameterSetName = 'Full')]
         [Parameter(Mandatory = $true, ParameterSetName = 'SuccessOnly')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Minimal')]
         [string]$SID,
 
-        [Parameter(ParameterSetName = 'Full')]
-        [string]$ProfilePath = $null,
+        # Full parameter set properties
+        [Parameter(Mandatory = $true, ParameterSetName = 'Full')]
+        [string]$ProfilePath,
 
         [Parameter(Mandatory = $true, ParameterSetName = 'Full')]
         [Parameter(Mandatory = $true, ParameterSetName = 'SuccessOnly')]
-        [bool]$DeletionSuccess = $false,
+        [bool]$DeletionSuccess,
 
-        [Parameter(ParameterSetName = 'Full')]
-        [string]$DeletionMessage = $null,
+        [Parameter(Mandatory = $true, ParameterSetName = 'Full')]
+        [string]$DeletionMessage,
 
-        [Parameter(ParameterSetName = 'Full')]
-        [string]$ComputerName = $env:COMPUTERNAME
+        [Parameter(Mandatory = $true, ParameterSetName = 'Full')]
+        [string]$ComputerName
     )
 
     switch ($PSCmdlet.ParameterSetName)
