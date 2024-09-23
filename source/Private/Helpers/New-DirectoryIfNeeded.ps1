354c57fc0522706639e24ba87c09e974ce1f87d6
LarryWisherMan <MikeRow@upkep.net>
2024-09-23 12:05:15 -0700

    add helper function `New-DirectoryIfNeeded`

--- source/Private/Helpers/New-DirectoryIfNeeded.ps1
+++ source/Private/Helpers/New-DirectoryIfNeeded.ps1
@@ -1,3 +1,35 @@
+<#
+.SYNOPSIS
+Creates a directory if it does not already exist.
+
+.DESCRIPTION
+The `New-DirectoryIfNeeded` function checks if the specified directory exists. If it doesn't, the function will create the directory and return the created directory object. If the directory already exists, the function returns `$true`. In case of any errors during directory creation, the function returns `$false` and logs the error.
+
+.PARAMETER Directory
+Specifies the full path of the directory to check or create. This parameter is mandatory. If the directory path is `null`, empty, or contains only whitespace, the function throws an error.
+
+.EXAMPLE
+New-DirectoryIfNeeded -Directory 'C:\Temp\NewFolder'
+
+Description:
+This command checks if the directory 'C:\Temp\NewFolder' exists. If it doesn't, the directory will be created. If the directory already exists, the function will return `$true`.
+
+.EXAMPLE
+New-DirectoryIfNeeded -Directory 'D:\Logs'
+
+Description:
+This command checks if the directory 'D:\Logs' exists. If it does not, the function will create the directory. If the directory already exists, it returns `$true`.
+
+.EXAMPLE
+$directory = New-DirectoryIfNeeded -Directory 'C:\Data\Reports'
+
+Description:
+This command attempts to create the directory 'C:\Data\Reports' if it doesn't exist and assigns the result to `$directory`. If successful, `$directory` will contain the created directory object. If the directory already exists, `$true` will be assigned to `$directory`.
+
+.NOTES
+If the directory path is invalid or if an error occurs during the creation process, the function writes an error message and returns `$false`.
+#>
+
 function New-DirectoryIfNeeded
 {
     param (
@@ -7,12 +39,20 @@ function New-DirectoryIfNeeded
 
     try
     {
+        # Check if the Directory parameter is null or an empty string
+        if ([string]::IsNullOrWhiteSpace($Directory))
+        {
+            throw [System.ArgumentException]::new("The 'Directory' parameter cannot be null or empty.")
+        }
+
+        # If the directory does not exist, attempt to create it
         if (-not (Test-Path -Path $Directory))
         {
-            # Attempt to create the directory if it doesn't exist
             $newDirectory = New-Item -Path $Directory -ItemType Directory -Force -ErrorAction Stop
             return $newDirectory
         }
+
+        # If the directory exists, return $true
         return $true
     }
     catch
